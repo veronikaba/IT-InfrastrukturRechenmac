92a66b51bdd1e99e2552dff2da79ab01aba24f24
Andrea Huber <andrea.huber@idealo.de>
2017-04-06 11:09:12 +0200

    Initialer commit

---
+++ src/main/java/Maschine.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Maschine {
+
+    public static void main(String[] args) {
+	    Rahmen rahmen = new Rahmen();
+        rahmen.initGUI();
+    }
+}
---
+++ src/main/java/Rahmen.java
@@ -0,0 +1,15 @@
+package main.java;
+
+
+import javax.swing.*;
+
+public class Rahmen extends JFrame {
+
+    protected void initGUI() {
+        setTitle("Supertolle Elektro-Rechenmaschine");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
