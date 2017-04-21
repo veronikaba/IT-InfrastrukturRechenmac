e06bc2d25807e3c31be3b3a3500f5876c1d3ea12
Praskovia <s0554193@htw-berlin.de>
2017-04-21 14:27:22 +0200

    minus panel rot

--- src/main/java/elektro/rechen/Rahmen.java
+++ src/main/java/elektro/rechen/Rahmen.java
@@ -56,7 +56,7 @@ public class Rahmen extends JFrame {
 
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
-        panel.setBackground(Color.red);
+        panel.setBackground(Color.yellow);
 
         panel.add(textFieldPlus1);
         panel.add(labelPlus);
@@ -99,6 +99,7 @@ public class Rahmen extends JFrame {
 
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
+        panel.setBackground(Color.red);
 
         panel.add(textFieldMinus1);
         panel.add(labelPlus);
